3255074983a2373be151eba7a50fcf23b8a57665
jeff <xu_xu73@aliyun.com>
2015-04-23 22:18:07 +0800

    Implemented Devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   root 'pages#home'
-  get "about" =>"pages#about" #creat about path
+  get "about" =>"pages#about"
+   #creat about path
+  devise_scope :user do
+  get "/users/sign_out" => "devise/sessions#destroy"
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out wit/users/sign_outh "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
